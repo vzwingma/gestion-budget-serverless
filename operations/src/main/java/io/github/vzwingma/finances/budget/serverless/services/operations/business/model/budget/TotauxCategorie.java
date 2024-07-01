a3ca4bdb9750f372f3e3b8ce2205b67c477ba464
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2024-07-01 23:42:13 +0200

    corrections soldes arrondie à 2 digits

--- operations/src/main/java/io/github/vzwingma/finances/budget/serverless/services/operations/business/model/budget/TotauxCategorie.java
+++ operations/src/main/java/io/github/vzwingma/finances/budget/serverless/services/operations/business/model/budget/TotauxCategorie.java
@@ -7,6 +7,8 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * Totaux par catégories
@@ -29,12 +31,26 @@ public class TotauxCategorie implements Serializable {
     private Double totalAtFinMoisCourant = 0D;
 
 
+    /**
+     * Cette méthode est utilisée pour ajouter un montant au total actuel à ce jour.
+     * Le total est ensuite arrondi à deux décimales en utilisant la méthode HALF_UP pour l'arrondissement.
+     *
+     * @param montantAAjouter Le montant à ajouter au total actuel.
+     */
     public void ajouterATotalAtMaintenant(Double montantAAjouter) {
-        this.totalAtMaintenant += montantAAjouter;
+        BigDecimal bd = new BigDecimal(this.totalAtMaintenant + montantAAjouter).setScale(2, RoundingMode.HALF_UP);
+        this.totalAtMaintenant = bd.doubleValue();
     }
 
+    /**
+     * Cette méthode est utilisée pour ajouter un montant au total actuel à la fin du mois en cours.
+     * Le total est ensuite arrondi à deux décimales en utilisant la méthode HALF_UP pour l'arrondissement.
+     *
+     * @param montantAAjouter Le montant à ajouter au total actuel.
+     */
     public void ajouterATotalAtFinMoisCourant(Double montantAAjouter) {
-        this.totalAtFinMoisCourant += montantAAjouter;
+        BigDecimal bd = new BigDecimal(this.totalAtFinMoisCourant+ montantAAjouter).setScale(2, RoundingMode.HALF_UP);
+        this.totalAtFinMoisCourant = bd.doubleValue();
     }
 
     @Override
