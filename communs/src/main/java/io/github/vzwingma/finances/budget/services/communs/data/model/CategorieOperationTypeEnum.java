f4c0b4ed43dd11366e4470c28aa8dc706027649e
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2026-02-04 09:55:11 +0100

    Add missing commas to CategorieOperationTypeEnum entries

--- communs/src/main/java/io/github/vzwingma/finances/budget/services/communs/data/model/CategorieOperationTypeEnum.java
+++ communs/src/main/java/io/github/vzwingma/finances/budget/services/communs/data/model/CategorieOperationTypeEnum.java
@@ -17,6 +17,9 @@ public enum CategorieOperationTypeEnum {
     // plaisir
     PLAISIR,
     // épargne
-    ECONOMIES
-
+    ECONOMIES,
+    // imprévus
+    IMPREVUS,
+    // extras
+    EXTRAS;
 }
