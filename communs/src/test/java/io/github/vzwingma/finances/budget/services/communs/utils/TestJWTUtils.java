bceccf6084336645669b8c6e3f60e4b9e3359de9
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2026-03-29 23:17:04 +0200

    Refactor TestJWTUtils to simplify timestamp assertions and remove unused imports

--- communs/src/test/java/io/github/vzwingma/finances/budget/services/communs/utils/TestJWTUtils.java
+++ communs/src/test/java/io/github/vzwingma/finances/budget/services/communs/utils/TestJWTUtils.java
@@ -11,8 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -96,10 +96,10 @@ class TestJWTUtils {
         assertNotNull(token.getPayload());
         assertEquals("https://accounts.google.com", token.getPayload().getIss());
         assertNotNull(token.issuedAt());
-        ZoneId parisZoneId = ZoneId.of("Europe/Paris");
-        assertEquals(LocalDateTime.of(2023, 1, 2, 12, 46, 42).atZone(parisZoneId), token.issuedAt().atZone(parisZoneId));
+        assertEquals(1672660002L, token.getPayload().getIat());
         assertNotNull(token.expiredAt());
-        assertEquals(LocalDateTime.of(2023, 1, 2, 13, 46, 42).atZone(parisZoneId), token.expiredAt().atZone(parisZoneId));
+        assertEquals(1672663602L, token.getPayload().getExp());
+        assertEquals(Duration.ofHours(1), Duration.between(token.issuedAt(), token.expiredAt()));
 
         LOG.info(LocalDateTime.now().toString());
         LOG.info(token.expiredAt().toString());
